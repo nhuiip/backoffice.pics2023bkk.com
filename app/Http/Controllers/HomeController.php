8eb089a95606c8e79b0777b71447b36edfc1ad40
Preedarat Chuthong <preedarat.ch@Preedarats-MacBook-Pro.local>
2023-08-15 02:30:36 +0700

    asjust dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $breadcrumbs = [
+            ['route' => '', 'name' => 'Dashboard Summary'],
+        ];
+        $count_member_all = Member::count();
+        $count_member_paid = Member::where('payment_status', 2)->count();
+        $count_member_pending = Member::where('payment_status', 1)->count();
+        $paid_per_country = DB::table('members')->select('country', DB::raw('COUNT(country) as countCountry'))->where('payment_status', 2)->groupBy('country')->get();
+        $paid_per_organization = DB::table('members')->select('organization', DB::raw('COUNT(organization) as countOrganization'))->where('payment_status', 2)->groupBy('organization')->get();
+        // dd($paid_per_organization);
+        return view('home', [
+            'title' => 'Dashboard',
+            'breadcrumbs' => $breadcrumbs,
+            'count_member_all' => $count_member_all,
+            'count_member_paid' => $count_member_paid,
+            'count_member_pending' => $count_member_pending,
+            'paid_per_country' => $paid_per_country,
+            'paid_per_organization' => $paid_per_organization,
+        ]);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -168,6 +168,20 @@
                                             class="fa fa-angle-right ps-2" aria-hidden="true"> </i></div>
                                 </li>
                                 <li class="sidebar-list">
+                                    <a class="sidebar-link sidebar-title link-nav @if (Route::is('home')) menu-active @endif"
+                                        href="{{ route('home') }}">
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g>
+                                              <g>
+                                                <path d="M9.07861 16.1355H14.8936" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.3999 13.713C2.3999 8.082 3.0139 8.475 6.3189 5.41C7.7649 4.246 10.0149 2 11.9579 2C13.8999 2 16.1949 4.235 17.6539 5.41C20.9589 8.475 21.5719 8.082 21.5719 13.713C21.5719 22 19.6129 22 11.9859 22C4.3589 22 2.3999 22 2.3999 13.713Z" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                              </g>
+                                            </g>
+                                          </svg>
+                                        <span>Dashboard</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-list">
                                     <a class="sidebar-link sidebar-title link-nav @if (Route::is('members.*')) menu-active @endif"
                                         href="{{ route('members.index') }}">
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -194,31 +208,6 @@
                                         <span>Registrant</span>
                                     </a>
                                 </li>
-                                {{-- <li class="sidebar-list">
-                                    <a class="sidebar-link sidebar-title link-nav @if (Route::is('banners.*')) menu-active @endif"
-                                        href="{{ route('banners.index') }}">
-                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <g>
-                                                <g>
-                                                    <path
-                                                        d="M6.07056 16.4588C6.07056 16.4588 6.88256 14.8218 8.06456 14.8218C9.24656 14.8218 9.85056 16.1968 11.1606 16.1968C12.4696 16.1968 13.9386 12.7488 15.4226 12.7488C16.9046 12.7488 17.9706 15.1398 17.9706 15.1398"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                                        d="M10.1393 9.10487C10.1393 9.96487 9.44229 10.6629 8.58129 10.6629C7.72129 10.6629 7.02429 9.96487 7.02429 9.10487C7.02429 8.24487 7.72129 7.54688 8.58129 7.54688C9.44229 7.54788 10.1393 8.24487 10.1393 9.10487Z"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                                        d="M2.75024 12C2.75024 18.937 5.06324 21.25 12.0002 21.25C18.9372 21.25 21.2502 18.937 21.2502 12C21.2502 5.063 18.9372 2.75 12.0002 2.75C5.06324 2.75 2.75024 5.063 2.75024 12Z"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                </g>
-                                            </g>
-                                        </svg>
-                                        <span>Banners</span>
-                                    </a>
-                                </li> --}}
                                 <li class="sidebar-list">
                                     <a class="sidebar-link sidebar-title link-nav @if (Route::is('news.*')) menu-active @endif"
                                         href="{{ route('news.index') }}">
@@ -388,44 +377,44 @@
                                         <span>Settings</span>
                                     </a>
                                 </li>
-                                @if(Auth::user()->email == 'admin@pics2023bkk.com')
-                                <li class="sidebar-list">
-                                    <a class="sidebar-link sidebar-title link-nav @if (Route::is('users.*')) menu-active @endif"
-                                        href="{{ route('users.index') }}">
-                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <g>
+                                @if (Auth::user()->email == 'admin@pics2023bkk.com')
+                                    <li class="sidebar-list">
+                                        <a class="sidebar-link sidebar-title link-nav @if (Route::is('users.*')) menu-active @endif"
+                                            href="{{ route('users.index') }}">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
                                                 <g>
-                                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                                        d="M11.9724 20.3683C8.73343 20.3683 5.96643 19.8783 5.96643 17.9163C5.96643 15.9543 8.71543 14.2463 11.9724 14.2463C15.2114 14.2463 17.9784 15.9383 17.9784 17.8993C17.9784 19.8603 15.2294 20.3683 11.9724 20.3683Z"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                                        d="M11.9725 11.4488C14.0985 11.4488 15.8225 9.72576 15.8225 7.59976C15.8225 5.47376 14.0985 3.74976 11.9725 3.74976C9.84645 3.74976 8.12245 5.47376 8.12245 7.59976C8.11645 9.71776 9.82645 11.4418 11.9455 11.4488H11.9725Z"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                    <path
-                                                        d="M18.3622 10.3915C19.5992 10.0605 20.5112 8.93254 20.5112 7.58954C20.5112 6.18854 19.5182 5.01854 18.1962 4.74854"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                    <path
-                                                        d="M18.9431 13.5444C20.6971 13.5444 22.1951 14.7334 22.1951 15.7954C22.1951 16.4204 21.6781 17.1014 20.8941 17.2854"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                    <path
-                                                        d="M5.58372 10.3915C4.34572 10.0605 3.43372 8.93254 3.43372 7.58954C3.43372 6.18854 4.42772 5.01854 5.74872 4.74854"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
-                                                    <path
-                                                        d="M5.00176 13.5444C3.24776 13.5444 1.74976 14.7334 1.74976 15.7954C1.74976 16.4204 2.26676 17.1014 3.05176 17.2854"
-                                                        stroke="#130F26" stroke-width="1.5" stroke-linecap="round"
-                                                        stroke-linejoin="round"></path>
+                                                    <g>
+                                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                                            d="M11.9724 20.3683C8.73343 20.3683 5.96643 19.8783 5.96643 17.9163C5.96643 15.9543 8.71543 14.2463 11.9724 14.2463C15.2114 14.2463 17.9784 15.9383 17.9784 17.8993C17.9784 19.8603 15.2294 20.3683 11.9724 20.3683Z"
+                                                            stroke="#130F26" stroke-width="1.5"
+                                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                                            d="M11.9725 11.4488C14.0985 11.4488 15.8225 9.72576 15.8225 7.59976C15.8225 5.47376 14.0985 3.74976 11.9725 3.74976C9.84645 3.74976 8.12245 5.47376 8.12245 7.59976C8.11645 9.71776 9.82645 11.4418 11.9455 11.4488H11.9725Z"
+                                                            stroke="#130F26" stroke-width="1.5"
+                                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path
+                                                            d="M18.3622 10.3915C19.5992 10.0605 20.5112 8.93254 20.5112 7.58954C20.5112 6.18854 19.5182 5.01854 18.1962 4.74854"
+                                                            stroke="#130F26" stroke-width="1.5"
+                                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path
+                                                            d="M18.9431 13.5444C20.6971 13.5444 22.1951 14.7334 22.1951 15.7954C22.1951 16.4204 21.6781 17.1014 20.8941 17.2854"
+                                                            stroke="#130F26" stroke-width="1.5"
+                                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path
+                                                            d="M5.58372 10.3915C4.34572 10.0605 3.43372 8.93254 3.43372 7.58954C3.43372 6.18854 4.42772 5.01854 5.74872 4.74854"
+                                                            stroke="#130F26" stroke-width="1.5"
+                                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        <path
+                                                            d="M5.00176 13.5444C3.24776 13.5444 1.74976 14.7334 1.74976 15.7954C1.74976 16.4204 2.26676 17.1014 3.05176 17.2854"
+                                                            stroke="#130F26" stroke-width="1.5"
+                                                            stroke-linecap="round" stroke-linejoin="round"></path>
+                                                    </g>
                                                 </g>
-                                            </g>
-                                        </svg>
-                                        <span>Users</span>
-                                    </a>
-                                </li>
+                                            </svg>
+                                            <span>Users</span>
+                                        </a>
+                                    </li>
                                 @endif
                             </ul>
                         </div>
